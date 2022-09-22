7280f737f9dfdceed1404e3b54308fdd745b74be
Brian Lambert <brianlambert2408@gmail.com>
2022-09-23 08:38:49 +1000

    Completed tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
+const endpoint = 'http://localhost:3000/movies';
+
 const init = () => {
-  
+    const form = document.querySelector("form");
+    const title = document.querySelector('section#movieDetails h4');
+    const summary = document.querySelector('section#movieDetails p');
+    
+    form.addEventListener("submit", (e)=>{
+        e.preventDefault();
+
+        const val = searchByID.value;
+
+        if(!val || !Number(val)){ return ;}
+
+        
+        fetch(`${endpoint}/${val}`)
+        .then(res=>{
+            if(!res.ok){ throw Error; }
+            return res.json();
+        })
+        .then(json=>{
+            title.innerText = json.title;
+            summary.innerText = json.summary;
+        })
+        .catch(err=>{
+            title.innerText = "Error";
+            summary.innerText = "ID not found!";
+        })
+        
+    });
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
